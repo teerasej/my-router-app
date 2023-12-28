4c62db376b63e9d63b91568173827b6ccfd5e367
Teerasej <teerasej@gmaill.com>
2023-12-28 11:52:16 +0700

    add menu and sample pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Setting } from './pages/Setting'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Menu</h1>
+        <Link to="/">Home</Link>
+        <Link to="/setting">Setting</Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/setting" Component={Setting} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/pages/Setting.tsx
@@ -0,0 +1,9 @@
+
+
+export const Setting = () => {
+    return (
+      <div>Setting</div>
+    )
+  }
+  
+  
